8d4c602b998c69cd44a2c3feea28a8360a756c0f
Caleb Priest <vinyl1225@gmail.com>
2019-04-01 17:52:57 -0400

    initial commit.

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route('/')
+def my_form():
+    return render_template("home.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    wage = request.form['wage']
+    hours = request.form['hours']
+    multiply_text = int(hours) * int(wage)
+    return str(multiply_text)
+if __name__ == '__main__':
+    app.run(debug=True)
